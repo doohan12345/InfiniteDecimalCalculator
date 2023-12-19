b1da7f97756508b93c4ab19386667e9c184cbb9a
doohan12345 <152001787+doohan12345@users.noreply.github.com>
2023-12-20 02:16:32 +0900

    Update compare_size.h

--- compare_size.h
+++ compare_size.h
@@ -1,15 +1,9 @@
-int max(int a, int b) 
-{
-    if (a <= b)
-        return b;
-    else
-        return a;
+int max(int a, int b) {
+    // 만약 a가 b보다 작거나 같다면 b를 반환하고, 그렇지 않다면 a를 반환합니다.
+    return (a <= b) ? b : a;
 }
 
-int min(int a, int b)
-{
-    if (a <= b)
-        return a;
-    else
-        return b;
+int min(int a, int b) {
+    // 만약 a가 b보다 작거나 같다면 a를 반환하고, 그렇지 않다면 b를 반환합니다.
+    return (a <= b) ? a : b;
 }
